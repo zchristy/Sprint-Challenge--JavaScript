ff854e87aa7661b0be16311b0b80cada9cd25dde
zchristy <zchristy44@gmail.com>
2019-04-05 12:04:07 -0500

    tweaks

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,4 +1,4 @@
- console.log(`==== Objects ====`); 
+ console.log(`==== Objects ====`);
 
 /*
   Given the following information about dinosaurs, create 3 objects:
@@ -70,23 +70,23 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 
 // Once you have the new array created, sort the universities alphabetically and log the result. */
-// const universities = [];
-// for(let i = 0; i < graduates.length; i++){
-//   universities.push(graduates[i].university);
-// }
-// console.log(universities.sort());
-//
-// /* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
-//
-// The resulting contact information should have a space between the first name and the email information like this:
-// Name email@example.com
-//
-// Log the result of your new array. */
-// const contactInfo = [];
-// for(let i = 0; i < graduates.length; i++){
-//   contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
-// }
-// console.log(contactInfo);
+const universities = [];
+for(let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort());
+
+/* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
+
+The resulting contact information should have a space between the first name and the email information like this:
+Name email@example.com
+
+Log the result of your new array. */
+const contactInfo = [];
+for(let i = 0; i < graduates.length; i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
+console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
